81a4ac88744650e7836233967c23ce9aa119da63
Sam Sunani <lakshmn.sunani@gmail.com>
2017-11-10 14:29:57 -0800

    Changed location to lower right

--- amp-titleOverlay.js
+++ amp-titleOverlay.js
@@ -3,13 +3,13 @@
 (function (mediaPlayer) {
     "use strict";
 
-    mediaPlayer.plugin('titleOverlay', function (options) {
+    mediaPlayer.plugin('videoOverlayTool', function (options) {
         var player = this,
             name = !!options && !!options.name ? options.name : '',
             opacity = !!options && !!options.opacity ? options.opacity : 1,
             horizontalPosition = !!options && !!options.horizontalPosition ? options.horizontalPosition : 'left',
             verticalPosition = !!options && !!options.verticalPosition ? options.verticalPosition : 'top',
-            contentTitleCssClass = 'amp-title-overlay';
+            contentTitleCssClass = 'amp-video-overlay-tool';
 
         var Component = mediaPlayer.getComponent('Component');
 
@@ -45,42 +45,10 @@
             return position;
         }
 
-        function updateContentTitleMaxSize(logoElement, logoSpan) {
-            // Update image max size acording video size
-            var videoElement = player.el();
-            if ((videoElement.clientHeight < logoSpan.parentElement.clientHeight) || (videoElement.clientWidth < logoSpan.parentElement.clientWidth)) {
-                logoSpan.style.maxHeight = videoElement.clientHeight + 'px';
-                logoSpan.style.maxWidth = videoElement.clientWidth + 'px';
-            } else {
-                logoSpan.style.maxHeight = '100%';
-                logoSpan.style.maxWidth = '100%';
-            }
-        }
-
-        function updateContentTitlePosition(logoElement, logoSpan) {
-            // Update DIV based on image values (now calculated because it was added to the DOM)
-            logoElement.style.left = getLogoHorizontalPosition(logoSpan, horizontalPosition) + 'px';
-            logoElement.style.top = getLogoVerticalPosition(logoSpan, verticalPosition) + 'px';
-        }
-
         function updateContentTitle() {
             // Fix to Logo position when the video returns from full screen
-            player.contentTitle.container.style.left = '0';
-            player.contentTitle.container.style.top = '0';
-            updateContentTitleMaxSize(player.contentTitle.container, player.contentTitle.span);
-            updateContentTitlePosition(player.contentTitle.container, player.contentTitle.span);
-        }
-
-        function showContentTitle() {
-            updateContentTitle();
-
-            player.contentTitle.removeClass("vjs-user-inactive");
-            player.contentTitle.addClass("vjs-user-active");
-        }
-
-        function hideContentTitle() {
-            player.contentTitle.removeClass("vjs-user-active");
-            player.contentTitle.addClass("vjs-user-inactive");
+            player.contentTitle.container.style.right = '0px';
+            player.contentTitle.container.style.bottom = '0px';
         }
 
         // Create Logo
@@ -98,7 +66,6 @@
                 updateContentTitle(); 
             };
             
-            var span = videojs.createEl('span', {});
             var div = videojs.createEl('div', {});
             div.style.cssText='height: 50px; width: 50px;';
             div.className = "indicator";
@@ -107,9 +74,7 @@
             };
 
             el.appendChild(div);       
-            el.appendChild(span);
             this.container = el;
-            this.span = span;
 
             return el;
         };
@@ -122,9 +87,6 @@
             
             player.on(mediaPlayer.eventName.fullscreenchange, updateContentTitle);
             player.on("resize", updateContentTitle);
-            
-            player.on("useractive", showContentTitle);
-            player.on("userinactive", hideContentTitle);
                         
             updateContentTitle();
             
